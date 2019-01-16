4ea332e61bc4adefa2d5d51f59017efc1f537593
Enrico Bottani <enrico.bottani@hotmail.com>
2019-01-16 17:36:39 +0100

    Commit esame

--- src/main/java/ch/supsi/webapp/web/controller/DynamicBlogController.java
+++ src/main/java/ch/supsi/webapp/web/controller/DynamicBlogController.java
@@ -1,5 +1,7 @@
 package ch.supsi.webapp.web.controller;
 
+import ch.supsi.webapp.web.model.entity.Category;
+import ch.supsi.webapp.web.model.entity.Comment;
 import ch.supsi.webapp.web.model.page.*;
 import ch.supsi.webapp.web.model.entity.BlogPost;
 import ch.supsi.webapp.web.service.BlogPostService;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.security.core.userdetails.User;
 
@@ -30,6 +33,16 @@ public class DynamicBlogController {
         model.addAttribute("page", new IndexPage());
         return "index";
     }
+
+    @GetMapping("/cancellati")
+    public String cancellati(Model model) {
+        long numOfPosts = blogPostService.numOfPosts();
+        if (numOfPosts == 0) model.addAttribute("posts", new ArrayList<BlogPost>());
+        else model.addAttribute("posts", blogPostService.getCancellati((int) numOfPosts));
+        model.addAttribute("page", new IndexPage());
+        return "cancellati";
+    }
+
 
     // Ritorna la pagina di creazione di un BlogPost.
     @GetMapping("/blog/new")
@@ -51,14 +64,25 @@ public class DynamicBlogController {
 
     @GetMapping("/blog/{id}")
     public String newBlogPost(@PathVariable long id, Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        //(Si ê debole, avevo fretta D: )
+        if (blogPostService.get(id).isDeleted() && !user.getUsername().equals("admin"))
+            return "redirect:/sorry";
         model.addAttribute("blogPost", blogPostService.get(id));
         model.addAttribute("page", new DetailsPage());
         return "details";
     }
 
+    @GetMapping("/sorry")
+    public String asd() {
+        return "sorry";
+    }
+
     @GetMapping("/blog/{id}/edit")
     public String editBlogPost(@PathVariable long id, Model model) {
         model.addAttribute("blogPost", blogPostService.get(id));
+        if (blogPostService.get(id).isDeleted())
+            return "redirect:/sorry";
         model.addAttribute("users", blogPostService.getAllUsers());
         model.addAttribute("categories", blogPostService.getAllCategories());
         model.addAttribute("page", new EditPage());
@@ -97,5 +121,28 @@ public class DynamicBlogController {
     public String register(ch.supsi.webapp.web.model.entity.User newUser, Model model) {
         blogPostService.addUser(newUser);
         return "redirect:/login";
+    }
+
+    @GetMapping("/search")
+    public String search(Model model) {
+        List<BlogPost> blogPosts = blogPostService.searchAll(new Category("Cultura"));
+        return "redirect:/search";
+    }
+
+    @PostMapping("/commenta")
+    public String commentaPost(ch.supsi.webapp.web.model.entity.User newUser, Model model) {
+        blogPostService.addUser(newUser);
+        return "redirect:/commenta";
+    }
+
+    @GetMapping("/commenta/{id}")
+    public String commentaGet(ch.supsi.webapp.web.model.entity.User newUser, @PathVariable long id, Model model) {
+        /*
+         * Non va...
+         * */
+
+        //model.addAttribute("user", new ch.supsi.webapp.web.model.entity.User());
+        model.addAttribute("comment", new Comment());
+        return "redirect:/commenta";
     }
 }
